ecd21a2681686874e06820142b7f7537b2d371f0
Drake Bott <me@drake.dev>
2025-10-02 02:46:05 +0000

    chore: run formatter

--- src/parser/metadata.rs
+++ src/parser/metadata.rs
@@ -1,6 +1,6 @@
-use rust_norg::metadata::{parse_metadata, NorgMeta};
 use rust_norg::NorgAST::{self, VerbatimRangedTag};
-use serde_json::{json, Map, Value};
+use rust_norg::metadata::{NorgMeta, parse_metadata};
+use serde_json::{Map, Value, json};
 use wasm_bindgen::prelude::*;
 
 pub fn extract_metadata(ast: &[NorgAST]) -> Value {
@@ -34,9 +34,10 @@ fn meta_to_json(meta: &NorgMeta) -> Value {
         Str(s) => json!(s),
         Num(n) => json!(n),
         Array(array) => json!(array.iter().map(meta_to_json).collect::<Vec<_>>()),
-        Object(map) => json!(map
-            .iter()
-            .map(|(key, value)| (key.clone(), meta_to_json(value)))
-            .collect::<Map<_, _>>()),
+        Object(map) => json!(
+            map.iter()
+                .map(|(key, value)| (key.clone(), meta_to_json(value)))
+                .collect::<Map<_, _>>()
+        ),
     }
 }
