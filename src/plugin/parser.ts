63eaf6363ac9d23c64443fed0edd54f1f366b8fd
Drake Bott <me@drake.dev>
2025-11-08 17:15:23 -0600

    Update napi config

--- src/plugin/parser.ts
+++
@@ -1,6 +0,0 @@
-import type { parseNorg } from '@parser';
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { parseNorg: parse } = require('../../dist/napi/index.node');
-
-export const parseNorgContent: typeof parseNorg = parse;
