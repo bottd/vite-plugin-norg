d196e15316bf897cf99f7ee21582a4d7ea74fa95
Drake Bott <me@drake.dev>
2025-12-02 14:23:25 -0600

    feat: export toc in generators

--- src/plugin/generators/react.ts
+++ src/plugin/generators/react.ts
@@ -1,13 +1,11 @@
 import type { NorgParseResult } from '@parser';
 
-export function generateReactOutput({ html, metadata }: NorgParseResult) {
-  const metadataJson = JSON.stringify(metadata ?? {});
-  const htmlJson = JSON.stringify(html);
-
+export function generateReactOutput({ html, metadata, toc }: NorgParseResult) {
   return [
     'import React from "react";',
-    `export const metadata = ${metadataJson};`,
-    `const htmlContent = ${htmlJson};`,
+    `export const metadata = ${JSON.stringify(metadata ?? {})};`,
+    `export const toc = ${JSON.stringify(toc ?? [])};`,
+    `const htmlContent = ${JSON.stringify(html)};`,
     'export const Component = () => React.createElement("div", { dangerouslySetInnerHTML: { __html: htmlContent } });',
     'export default Component;',
   ].join('\n');
