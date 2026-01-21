5585b2b7addc017e219301dd4d38d1c655bb3dfb
Drake Bott <me@drake.dev>
2026-01-20 20:49:01 -0600

    update .d.ts files

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,5 +1,9 @@
 export { norgPlugin, type ArboriumConfig } from './plugin';
 export type { NorgParseResult, TocEntry, InlineComponent } from '@parser';
+export type { HtmlModule } from './generators/html';
+export type { SvelteModule } from './generators/svelte';
+export type { ReactModule } from './generators/react';
+export type { VueModule } from './generators/vue';
 
 import { norgPlugin } from './plugin';
 export default norgPlugin;
